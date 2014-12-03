ccdbf30dfdfecda0d007cad67f9c111d1184ac93
Bill Ruddock <bill.ruddock@gmail.com>
2014-12-03 12:06:27 +0000

    Spec for binary recipe

--- spec/binary_spec.rb
+++ spec/binary_spec.rb
@@ -2,6 +2,17 @@ require 'spec_helper'
 
 describe 'wkhtmltopdf::binary' do
   let(:chef_run) do
-    ChefSpec::Runner.new.converge(described_recipe)
+    ChefSpec::SoloRunner.new.converge(described_recipe)
   end
+
+  let(:cache_dir) { Chef::Config[:file_cache_path] }
+  let(:version) { '0.12.1' }
+  let(:platform) { 'linux-precise' }
+  let(:architecture) { 'amd64' }
+  let(:suffix) { 'deb' }
+  let(:archive) { "wkhtmltox-#{version}_#{platform}-#{architecture}.#{suffix}" }
+  let(:download_dest) { File.join(cache_dir, archive) }
+
+  it { expect(chef_run).to create_remote_file_if_missing(download_dest) }
+  it { expect(chef_run).to install_package('wkhtmltox') }
 end
