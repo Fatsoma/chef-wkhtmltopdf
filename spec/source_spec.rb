80bcbcc5091b7a56d22f2fd96c7dbf8b529635b8
Bill Ruddock <bill.ruddock@gmail.com>
2014-12-04 01:00:55 +0000

    spec for source install

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -9,9 +9,11 @@ describe 'wkhtmltopdf::source' do
   end
 
   let(:cache_dir) { Chef::Config[:file_cache_path] }
-  let(:archive) { 'wkhtmltox-0.12.1.tar.bz2' }
+  let(:version) { '0.12.1' }
+  let(:archive) { "wkhtmltox-#{version}.tar.bz2" }
   let(:download_dest) { File.join(cache_dir, archive) }
   let(:install_dir) { '/test/bin' }
+  let(:extracted_path) { File.join(cache_dir, "wkhtmltox-#{version}") }
 
   it { expect(chef_run).to create_remote_file_if_missing(download_dest) }
   it 'extracts archive' do
@@ -21,18 +23,18 @@ describe 'wkhtmltopdf::source' do
   end
   it 'compiles wkhtmltox' do
     expect(chef_run).to run_execute('compile_wkhtmltox')
-      .with_cwd("#{cache_dir}/wkhtmltox")
-      .with_command('make')
+      .with_cwd(extracted_path)
+      .with_command('scripts/build.py posix-local')
   end
   it 'installs wkhtmltoimage' do
     expect(chef_run).to run_execute('install_wkhtmltoimage')
-      .with_cwd(cache_dir)
-      .with_command("cp wkhtmltox/bin/wkhtmltoimage #{install_dir}/wkhtmltoimage")
+      .with_cwd(extracted_path)
+      .with_command("cp bin/wkhtmltoimage #{install_dir}/wkhtmltoimage")
   end
   it 'installs wkhtmltopdf' do
     expect(chef_run).to run_execute('install_wkhtmltopdf')
-      .with_cwd(cache_dir)
-      .with_command("cp wkhtmltox/bin/wkhtmltopdf #{install_dir}/wkhtmltopdf")
+      .with_cwd(extracted_path)
+      .with_command("cp bin/wkhtmltopdf #{install_dir}/wkhtmltopdf")
   end
 
   context 'with lib_dir' do
@@ -44,14 +46,17 @@ describe 'wkhtmltopdf::source' do
       end.converge(described_recipe)
     end
 
-    it { expect(chef_run).to run_execute('install_wkhtmltox_so') }
+    it do
+      expect(chef_run).to run_execute('install_wkhtmltox_so')
+        .with_command("cp lib/libwkhtmltox.so.#{version} #{lib_dir}/libwkhtmltox.so.#{version}")
+    end
     it do
       expect(chef_run).to create_link("#{lib_dir}/libwkhtmltox.so.0.12")
-        .with_to("#{lib_dir}/libwkhtmltox.so.0.12.1")
+        .with_to("#{lib_dir}/libwkhtmltox.so.#{version}")
     end
     it do
       expect(chef_run).to create_link("#{lib_dir}/libwkhtmltox.so.0")
-        .with_to("#{lib_dir}/libwkhtmltox.so.0.12.1")
+        .with_to("#{lib_dir}/libwkhtmltox.so.#{version}")
     end
   end
 end
