51524fb1b1428eeb1a993badb31168570a5fc91b
Bill Ruddock <bill.ruddock@gmail.com>
2014-12-09 01:18:34 +0000

    Add pkgconf dependency for freebsd

--- attributes/default.rb
+++ attributes/default.rb
@@ -95,7 +95,7 @@ if node['wkhtmltopdf']['install_method'] == 'source'
     end
     default['wkhtmltopdf']['dependency_packages'] = %W(patch gcc-c++ fontconfig-devel freetype-devel libpng-devel zlib-devel #{jpeg_package} openssl-devel libX11-devel libXext-devel libXrender-devel libstdc++-devel glibc-devel)
   when 'freebsd'
-    default['wkhtmltopdf']['dependency_packages'] = %w(gcc fontconfig freetype2 jpeg png gmake libX11 libXext libXrender perl5 libiconv)
+    default['wkhtmltopdf']['dependency_packages'] = %w(gcc fontconfig freetype2 jpeg png pkgconf gmake libX11 libXext libXrender perl5 libiconv)
   end
   default['wkhtmltopdf']['suffix'] = 'tar.bz2'
   default['wkhtmltopdf']['platform'] = ''
